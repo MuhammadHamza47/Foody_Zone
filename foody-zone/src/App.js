9fb29b8879a499c3600571772e6d5216b3102cca
Muhammad Hamza <hafizhamza2247@gmail.com>
2023-10-04 14:09:55 +0500

    Foody Zone

---
+++ foody-zone/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import Herosection from './Components/Herosection';
+
+const App=()=> {
+  return (
+    <>
+    <Herosection/>
+    </>
+  );
+}
+
+export default App;
